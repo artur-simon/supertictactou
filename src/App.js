ccd2a4588760e6beeb54dba4df79cebe4bf4e00d
Mark Kop <mk.marcelo.mk@gmail.com>
2020-03-11 21:45:25 -0300

    fix: move to react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const text = 'aaa'
+  return (
+    <div className="App">
+      <button >{text}</button>
+    </div>
+  );
+}
+
+export default App;
